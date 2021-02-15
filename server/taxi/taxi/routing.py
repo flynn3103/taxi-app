f7f1296af6603137819faec18756b903ca7485a8
flynn <trannhatlinh3103@gmail.com>
2021-02-15 16:12:01 +0700

    add middleware

--- server/taxi/taxi/routing.py
+++ server/taxi/taxi/routing.py
@@ -1,3 +1,16 @@
-from channels.routing import ProtocolTypeRouter
+# taxi/routing.py
 
-application = ProtocolTypeRouter({})
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from taxi.middleware import TokenAuthMiddlewareStack  # new
+from trips.consumers import TaxiConsumer
+
+
+application = ProtocolTypeRouter({
+    'websocket': TokenAuthMiddlewareStack(  # changed
+        URLRouter([
+            path('taxi/', TaxiConsumer),
+        ])
+    ),
+})
